a585a56543a83c33e023436967073416e351bfe5
jadessu <junioradessu@gmail.com>
2021-06-28 11:35:24 -0500

    add cache element references

---
+++ JS/htmlquiz.js
@@ -0,0 +1,72 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+
+
+
+
+
+
+
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+
+
+
+
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+const review = document.querySelector(".review")
+const options = document.querySelector(".answers").children
+const questionOn = document.querySelector(".questionOutOf")
+const question=document.querySelector(".question")
+const totalQuestions =document.querySelector(".totalQuestions")
+const correctAnswers =document.querySelector(".correctAnswers")
+const totalQuestions2 =document.querySelector(".totalQuestions2")
+const percentage =document.querySelector(".percentage")
+
+const nextButton = document.querySelector(".btn")
+const tryAgainButton = document.querySelector("#tryAgainButton")
+
+
+const firstAnswer = document.querySelector(".option1")
+const secondAnswer = document.querySelector(".option2")
+const thirdAnswer = document.querySelector(".option3")
+const fourthAnswer = document.querySelector(".option4")
+
+
+
+
+
+
+
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+
+
+
