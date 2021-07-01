2124fab5070d01d2d8f405b45b905d668aff1eb1
jadessu <junioradessu@gmail.com>
2021-06-30 19:46:19 -0500

    finish user style

--- JS/study.js
+++ JS/study.js
@@ -1,6 +1,7 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 const colorInput = document.querySelector("#change-it")
+const colorText = document.querySelector("#userColor")
 
 
 toggleButton.addEventListener('click', () => {
@@ -16,4 +17,17 @@ colorInput.addEventListener("click", addColor)
 function addColor(){
   let userColor = prompt("what color do you want the background to be?")
   document.body.style.backgroundColor = " "+ userColor +" "  
+  if (userColor === "yellow" || userColor === "white"){
+    colorText.style.color = "black"
+  
+  } else {
+    colorText.style.color = "white"
+  }
 }
+
+// function contrast(){
+// if (userColor === "yellow"){
+//   colorText.style.color = "black"
+
+// }
+// }
